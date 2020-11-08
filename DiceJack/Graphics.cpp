1ad3f3dcdfca44b7cb31db4cfb34d8cf57e00e3c
Anna Erkers <idril.celebrindal@gmail.com>
2020-11-08 10:29:06 +0100

    Change indentation for ASCII dice.

--- DiceJack/Graphics.cpp
+++ DiceJack/Graphics.cpp
@@ -7,46 +7,46 @@ void renderDivider() {
 void renderFirstDie(int value) {
 	switch (value) {
 	case 1:
-		std::cout << "  +-------+\n";
-		std::cout << "  |       |\n";
-		std::cout << "  |   *   |\n";
-		std::cout << "  |       |\n";
-		std::cout << "  +-------+\n";
+		std::cout << "      +-------+\n";
+		std::cout << "      |       |\n";
+		std::cout << "      |   *   |\n";
+		std::cout << "      |       |\n";
+		std::cout << "      +-------+\n";
 		break;
 	case 2:
-		std::cout << "  +-------+\n";
-		std::cout << "  | *     |\n";
-		std::cout << "  |       |\n";
-		std::cout << "  |     * |\n";
-		std::cout << "  +-------+\n";
+		std::cout << "      +-------+\n";
+		std::cout << "      | *     |\n";
+		std::cout << "      |       |\n";
+		std::cout << "      |     * |\n";
+		std::cout << "      +-------+\n";
 		break;
 	case 3:
-		std::cout << "  +-------+\n";
-		std::cout << "  | *     |\n";
-		std::cout << "  |   *   |\n";
-		std::cout << "  |     * |\n";
-		std::cout << "  +-------+\n";
+		std::cout << "      +-------+\n";
+		std::cout << "      | *     |\n";
+		std::cout << "      |   *   |\n";
+		std::cout << "      |     * |\n";
+		std::cout << "      +-------+\n";
 		break;
 	case 4:
-		std::cout << "  +-------+\n";
-		std::cout << "  | *   * |\n";
-		std::cout << "  |       |\n";
-		std::cout << "  | *   * |\n";
-		std::cout << "  +-------+\n";
+		std::cout << "      +-------+\n";
+		std::cout << "      | *   * |\n";
+		std::cout << "      |       |\n";
+		std::cout << "      | *   * |\n";
+		std::cout << "      +-------+\n";
 		break;
 	case 5:
-		std::cout << "  +-------+\n";
-		std::cout << "  | *   * |\n";
-		std::cout << "  |   *   |\n";
-		std::cout << "  | *   * |\n";
-		std::cout << "  +-------+\n";
+		std::cout << "      +-------+\n";
+		std::cout << "      | *   * |\n";
+		std::cout << "      |   *   |\n";
+		std::cout << "      | *   * |\n";
+		std::cout << "      +-------+\n";
 		break;
 	case 6:
-		std::cout << "  +-------+\n";
-		std::cout << "  | *   * |\n";
-		std::cout << "  | *   * |\n";
-		std::cout << "  | *   * |\n";
-		std::cout << "  +-------+\n";
+		std::cout << "      +-------+\n";
+		std::cout << "      | *   * |\n";
+		std::cout << "      | *   * |\n";
+		std::cout << "      | *   * |\n";
+		std::cout << "      +-------+\n";
 		break;
 	default:
 		std::cout << "Error: Value must be between 1 and 6.\n";
@@ -56,46 +56,46 @@ void renderFirstDie(int value) {
 void renderSecondDie(int value) {
 	switch (value) {
 	case 1:
-		std::cout << "             +-------+\n";
-		std::cout << "             |       |\n";
-		std::cout << "             |   *   |\n";
-		std::cout << "             |       |\n";
-		std::cout << "             +-------+\n";
+		std::cout << "                 +-------+\n";
+		std::cout << "                 |       |\n";
+		std::cout << "                 |   *   |\n";
+		std::cout << "                 |       |\n";
+		std::cout << "                 +-------+\n";
 		break;
 	case 2:
-		std::cout << "             +-------+\n";
-		std::cout << "             | *     |\n";
-		std::cout << "             |       |\n";
-		std::cout << "             |     * |\n";
-		std::cout << "             +-------+\n";
+		std::cout << "                 +-------+\n";
+		std::cout << "                 | *     |\n";
+		std::cout << "                 |       |\n";
+		std::cout << "                 |     * |\n";
+		std::cout << "                 +-------+\n";
 		break;
 	case 3:
-		std::cout << "             +-------+\n";
-		std::cout << "             | *     |\n";
-		std::cout << "             |   *   |\n";
-		std::cout << "             |     * |\n";
-		std::cout << "             +-------+\n";
+		std::cout << "                 +-------+\n";
+		std::cout << "                 | *     |\n";
+		std::cout << "                 |   *   |\n";
+		std::cout << "                 |     * |\n";
+		std::cout << "                 +-------+\n";
 		break;
 	case 4:
-		std::cout << "             +-------+\n";
-		std::cout << "             | *   * |\n";
-		std::cout << "             |       |\n";
-		std::cout << "             | *   * |\n";
-		std::cout << "             +-------+\n";
+		std::cout << "                 +-------+\n";
+		std::cout << "                 | *   * |\n";
+		std::cout << "                 |       |\n";
+		std::cout << "                 | *   * |\n";
+		std::cout << "                 +-------+\n";
 		break;
 	case 5:
-		std::cout << "             +-------+\n";
-		std::cout << "             | *   * |\n";
-		std::cout << "             |   *   |\n";
-		std::cout << "             | *   * |\n";
-		std::cout << "             +-------+\n";
+		std::cout << "                 +-------+\n";
+		std::cout << "                 | *   * |\n";
+		std::cout << "                 |   *   |\n";
+		std::cout << "                 | *   * |\n";
+		std::cout << "                 +-------+\n";
 		break;
 	case 6:
-		std::cout << "             +-------+\n";
-		std::cout << "             | *   * |\n";
-		std::cout << "             | *   * |\n";
-		std::cout << "             | *   * |\n";
-		std::cout << "             +-------+\n";
+		std::cout << "                 +-------+\n";
+		std::cout << "                 | *   * |\n";
+		std::cout << "                 | *   * |\n";
+		std::cout << "                 | *   * |\n";
+		std::cout << "                 +-------+\n";
 		break;
 	default:
 		std::cout << "Error: Value must be between 1 and 6.\n";
