d9be154cdffe3a75bdc6ec5a8a41f1d3ec7dc0f6
Anna Erkers <idril.celebrindal@gmail.com>
2020-11-09 08:56:35 +0100

    Add game rules and victory and loss banners.

--- DiceJack/Graphics.h
+++ DiceJack/Graphics.h
@@ -1,7 +1,8 @@
 #pragma once
 
 void renderWelcomeBanner();
-void renderVictoryBanner();
+void renderVictoryBanner(std::string type);
+void renderLossBanner(std::string type);
 void renderDivider();
 void renderRollingDice();
 void renderFirstDie(int value);
